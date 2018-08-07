24582c4614e2612763fd5f78c17bc78fce1a224e
yukihane <yukihane.feather@gmail.com>
2018-08-07 17:09:09 +0900

    GetFullPathNameサンプル

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <windows.h>
+#include <tchar.h>
 //#include <fileapi.h>
 
 int main() {
-/*
   HANDLE handle = CreateFile("mydir..", 0, 0, NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS, NULL);
   printf("%s\n", (handle != INVALID_HANDLE_VALUE) ? "true":"false");
-*/
-  WIN32_FIND_DATA FindFileData;
-  HANDLE handle2 = FindFirstFile("mydir..\\*", &FindFileData);
+
+  LPTSTR lpFileName;
+  TCHAR lpBuffer[MAX_PATH+1]; 
+  GetFullPathName("mydir..\\xxx..", MAX_PATH+1, lpBuffer, &lpFileName); 
+  printf("fullPathName：%s\n", lpBuffer);
+  printf("fileName：%s\n", lpFileName);
+
+  WIN32_FIND_DATA findFileData  ;
+  HANDLE handle2 = FindFirstFile("mydir.\\xxx...", &findFileData);
   printf("%s\n", (handle2 != INVALID_HANDLE_VALUE) ? "true":"false");
+  if(handle2 != INVALID_HANDLE_VALUE) {
+    _tprintf("%s\n", findFileData.cFileName);
+    FindClose(handle2);
+  }
 
   return 0;
 }
